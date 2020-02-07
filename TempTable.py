9a2d78b756277b28a4e4b80b9711a86ed2746180
Roy Kedem <roy1kedem@gmail.com>
2020-02-07 19:08:40 +0200

    TempTable finished with Triple2 obj and test run main func

--- TempTable.py
+++ TempTable.py
@@ -1,5 +1,13 @@
 from Triple import Triple
 from KBucket import KBucket
+# todo: change name!!!!!!!!!
+
+
+class Triple2(Triple):
+    def __init__(self, triple, searched=False):
+        Triple.__init__(self, triple.ip, triple.port)
+        self.searched = searched
+        self.id = triple.id
 
 
 class TempTable:
@@ -7,9 +15,33 @@ class TempTable:
         self.node_lookup_table = []
 
     def add_sorted_bucket(self, k_bucket, my_id):
-        for triple in k_bucket.bucket:
-            for x in range(0, len(self.node_lookup_table)):
-                higher_then_before = triple.id ^ my_id > self.node_lookup_table[x] ^ my_id
-                lower_then_next = triple.id ^ my_id < self.node_lookup_table[x+1] ^ my_id
-                if higher_then_before and lower_then_next:
-                    self.node_lookup_table.insert(x, triple)
+        """
+        add a new bucket to the TempTable. it adds the triples in order by the distance from my_id
+        :param k_bucket: the bucket you want to add
+        :param my_id: the id of this node (to calc the distance from each triple)
+        :return:
+        """
+        for triple in k_bucket:
+            self.node_lookup_table.append(Triple2(triple))
+
+        self.node_lookup_table.sort(key=lambda elem: elem.id ^ my_id)
+
+
+if __name__ == '__main__':
+    my_id = 1
+    x = KBucket(2)
+    for i in range(40, 50):
+        x.add_triple(Triple('127.0.01', i))
+
+   # for triple in x.bucket:
+        # print("before - " + str(triple.id ^ my_id))
+
+    print("break")
+
+    t = TempTable()
+    t.add_sorted_bucket(x, my_id)
+    print(t.node_lookup_table)
+
+    for triple in t.node_lookup_table:
+        print("after - " + str(int((triple.id ^ my_id) / 10000000000000000000000000000000)))
+
